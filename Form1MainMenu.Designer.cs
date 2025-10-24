e3df2e368d99a14ed0faa39b9d1037752c54ec18
bugaenko-iv <iv.bugaenko@mail.ru>
2025-10-24 20:08:14 +0300

    Добавьте файлы проекта.

---
+++ Form1MainMenu.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Клавиатурный_тренажер_KeyboardMaster
+{
+    partial class Form1MainMenu
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1MainMenu));
+            this.panel1ControlForm1 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.guna2Button1CollapseApll = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button1ExitApll = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2ImageButton1 = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.panel1ControlForm1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1ControlForm1
+            // 
+            this.panel1ControlForm1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1ControlForm1.Controls.Add(this.panel1);
+            this.panel1ControlForm1.Location = new System.Drawing.Point(0, -1);
+            this.panel1ControlForm1.Name = "panel1ControlForm1";
+            this.panel1ControlForm1.Size = new System.Drawing.Size(1389, 35);
+            this.panel1ControlForm1.TabIndex = 0;
+            this.panel1ControlForm1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1ControlForm1_MouseDown);
+            this.panel1ControlForm1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1ControlForm1_MouseMove);
+            this.panel1ControlForm1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1ControlForm1_MouseUp);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.guna2Button1CollapseApll);
+            this.panel1.Controls.Add(this.guna2Button1ExitApll);
+            this.panel1.Location = new System.Drawing.Point(1267, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(119, 35);
+            this.panel1.TabIndex = 1;
+            // 
+            // guna2Button1CollapseApll
+            // 
+            this.guna2Button1CollapseApll.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button1CollapseApll.BorderColor = System.Drawing.Color.Transparent;
+            this.guna2Button1CollapseApll.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1CollapseApll.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1CollapseApll.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1CollapseApll.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1CollapseApll.FillColor = System.Drawing.Color.Transparent;
+            this.guna2Button1CollapseApll.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold);
+            this.guna2Button1CollapseApll.ForeColor = System.Drawing.Color.LightGray;
+            this.guna2Button1CollapseApll.Location = new System.Drawing.Point(16, 0);
+            this.guna2Button1CollapseApll.Name = "guna2Button1CollapseApll";
+            this.guna2Button1CollapseApll.Size = new System.Drawing.Size(50, 35);
+            this.guna2Button1CollapseApll.TabIndex = 1;
+            this.guna2Button1CollapseApll.Text = "—";
+            this.guna2Button1CollapseApll.Click += new System.EventHandler(this.guna2Button1CollapseApll_Click);
+            // 
+            // guna2Button1ExitApll
+            // 
+            this.guna2Button1ExitApll.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button1ExitApll.BorderColor = System.Drawing.Color.Transparent;
+            this.guna2Button1ExitApll.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1ExitApll.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1ExitApll.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1ExitApll.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1ExitApll.FillColor = System.Drawing.Color.Transparent;
+            this.guna2Button1ExitApll.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold);
+            this.guna2Button1ExitApll.ForeColor = System.Drawing.Color.LightGray;
+            this.guna2Button1ExitApll.HoverState.FillColor = System.Drawing.Color.RosyBrown;
+            this.guna2Button1ExitApll.Location = new System.Drawing.Point(69, 0);
+            this.guna2Button1ExitApll.Name = "guna2Button1ExitApll";
+            this.guna2Button1ExitApll.Size = new System.Drawing.Size(50, 35);
+            this.guna2Button1ExitApll.TabIndex = 0;
+            this.guna2Button1ExitApll.Text = "X";
+            this.guna2Button1ExitApll.Click += new System.EventHandler(this.guna2Button1ExitApll_Click);
+            // 
+            // guna2ImageButton1
+            // 
+            this.guna2ImageButton1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ImageButton1.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton1.Enabled = false;
+            this.guna2ImageButton1.HoverState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton1.Image = ((System.Drawing.Image)(resources.GetObject("guna2ImageButton1.Image")));
+            this.guna2ImageButton1.ImageOffset = new System.Drawing.Point(0, 0);
+            this.guna2ImageButton1.ImageRotate = 0F;
+            this.guna2ImageButton1.ImageSize = new System.Drawing.Size(210, 70);
+            this.guna2ImageButton1.Location = new System.Drawing.Point(40, 55);
+            this.guna2ImageButton1.Name = "guna2ImageButton1";
+            this.guna2ImageButton1.PressedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton1.Size = new System.Drawing.Size(231, 80);
+            this.guna2ImageButton1.TabIndex = 3;
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2PictureBox1.FillColor = System.Drawing.Color.Transparent;
+            this.guna2PictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox1.Image")));
+            this.guna2PictureBox1.ImageRotate = 0F;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(277, 55);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.Size = new System.Drawing.Size(279, 80);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2PictureBox1.TabIndex = 4;
+            this.guna2PictureBox1.TabStop = false;
+            this.guna2PictureBox1.UseTransparentBackground = true;
+            // 
+            // Form1MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(1389, 795);
+            this.Controls.Add(this.guna2PictureBox1);
+            this.Controls.Add(this.guna2ImageButton1);
+            this.Controls.Add(this.panel1ControlForm1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1MainMenu";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panel1ControlForm1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1ControlForm1;
+        private System.Windows.Forms.Panel panel1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1ExitApll;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1CollapseApll;
+        private Guna.UI2.WinForms.Guna2ImageButton guna2ImageButton1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+    }
+}
+
